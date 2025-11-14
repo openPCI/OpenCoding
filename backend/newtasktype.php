6bc103ce11fa70a4bfad585fea7911788168c91f
Jeppe Bundsgaard <jeppe@bundsgaard.net>
2025-11-14 11:53:14 +0100

    Add values to insert

--- backend/newtasktype.php
+++ backend/newtasktype.php
@@ -5,7 +5,7 @@ include_once($relative."dirs.php");
 include_once($shareddir."database.php");
 checkperm("opencodingadmin");
 
-$q='insert into tasktypes (tasktype_name,variables) values ("New tasktype","{}")';
+$q='insert into tasktypes (tasktype_name,variables,tasktype_description,playareatemplate,responseareatemplate,codeareatemplate,tasktype_instructions,insert_script,styles) values ("New tasktype","{}","","","","","","","") ON DUPLICATE KEY UPDATE tasktype_name=concat(tasktype_name," ","'.rand(1,1000).'");';
 
 $mysqli->query($q);
 $test_id=$mysqli->insert_id;
